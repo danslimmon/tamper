f495fae443d1ea4f6d2ab22bd2712d09f599d4f6
Dan Slimmon <dan.slimmon@gmail.com>
2013-08-09 21:53:48 +0000

    Latest using net, gonna try interfacing with http instead

--- server.js
+++ server.js
@@ -45,18 +45,97 @@ var Header = function(header_str) {
 };
 
 
+// An HTTP request.
+var Request = function() {
+    var data,
+        request_line,
+        content_length,
+        headers,
+        body;
+
+    // Parses the request headers and stores the objects in the `headers` attribute.
+    //
+    // Doesn't assume we have the body already, but does assume we have received all
+    // the headers.
+    var parseHeaders = function() {
+        var headers_str = data.split('\r\n\r\n')[0];
+
+        // Parse headers
+        var header_lines = headers_str.split('\r\n');
+        status_line = header_lines.shift();
+        headers = header_lines.map(function(h_str) {
+            hdr = Header(h_str);
+            if (hdr.normalizedName() == "content-length") { content_length = parseInt(hdr.value); }
+            return hdr;
+        });
+    }
+
+    // Parses the request headers and (possibly) body.
+    //
+    // Assumes the entire request has been received. If there is no request body, then
+    // the `body` attribute will be set to `null`.
+    var parse = function() {
+        headers = parseHeaders();
+        if (content_length !== undefined) {
+            // Break apart headers from body
+            var parts = data.split('\r\n\r\n', 2);
+            body = parts[1];
+            body = body.slice(0, content_length);
+        }
+    }
+
+    return {
+        // Adds data to the request.
+        addData: function(new_data) {
+            data += new_data;
+            parseHeaders();
+        },
+
+        // Determines whether the entire request has been received.
+        isOver: function() {
+            return (
+
+        // Returns an array of Header objects representing the request headers.
+        getHeaders: function() {
+            if (headers === undefined) { parse(); }
+            return headers;
+        },
+
+        // Sets the request headers to the given array of Header objects
+        setHeaders: function(new_headers) {
+            headers = new_headers;
+        },
+
+        // Returns the whole contents of the request that should be sent over the socket.
+        fullData: function() {
+            parse();
+            req_data = request_line + '\r\n';
+            req_data += headers.map(function(hdr) {
+                return hdr.protocolString();
+            }).join('\r\n');
+            
+            if (body !== undefined) {
+                req_data += '\r\n\r\n' + body;
+            }
+            return req_data;
+        }
+    }
+};
+
+
 // An HTTP response.
 var Response = function() {
-    var data = '';
-    var status_line = null;
-    var headers = null;
-    var body = null;
+    var data,
+        status_line,
+        headers,
+        body;
 
     // Parses the response data into headers and body
     var parseData = function() {
         // Break apart headers from body
         var parts = data.split('\r\n\r\n', 2);
         var headers_str = parts[0];
+        // This will set `body` to `undefined` if there's no body.
         body = parts[1];
 
         // Parse headers
@@ -75,7 +154,7 @@ var Response = function() {
 
         // Returns an array of Header objects representing the response headers.
         getHeaders: function() {
-            if (! headers) { parseData(); }
+            if (headers === undefined) { parseData(); }
             return headers;
         },
 
@@ -87,11 +166,14 @@ var Response = function() {
         // Returns the whole contents of the response that should be sent over the socket.
         fullData: function() {
             resp_data = status_line + '\r\n';
-            resp_data += headers.map(function(hdr) {
+            resp_data += getHeaders().map(function(hdr) {
                 return hdr.protocolString();
             }).join('\r\n');
-            resp_data += '\r\n\r\n' + body;
-            console.log('%j', resp_data);
+
+            if (body !== undefined) {
+                resp_data += '\r\n\r\n' + body;
+            }
+
             return resp_data;
         }
     }
@@ -136,6 +218,7 @@ var ResponseFilterSet = function() {
     }
 }
 
+
 // Process command-line arguments.
 var argv = optimist
     .usage('Proxies requests and tampers with them in supposedly-acceptable ways.\n\nUSAGE: node server.js [--listen-host HOST] [--listen-port PORT] [--dest-host HOST] [--dest-port PORT]')
@@ -156,11 +239,15 @@ if (argv['help'] || argv['h']) {
 // Define web server
 var server = net.createServer(function(client_socket) {
     var server_socket = new net.Socket();
+    var request = new Request();
     var response = new Response();
 
     server_socket.connect(argv['dest-port'], argv['dest-host']);
     client_socket.on('data', function(data) {
-        server_socket.write(data);
+        request.addData(data);
+        if (request.isOver()) {
+            server_socket.write(request.fullData());
+        }
     });
     client_socket.on('end', function(){
         server_socket.end();
